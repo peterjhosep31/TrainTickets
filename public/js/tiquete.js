e37a39015955f427dfd087aee2fefc646335c0c2
Pedro José Bermúdez López <bermudezlopezpedrojose@gmail.com>
2022-09-04 14:45:14 -0500

    Agregando validaciones para hacer la pagina mas segura

--- public/js/tiquete.js
+++ public/js/tiquete.js
@@ -2,6 +2,13 @@ function destino() {
   const destino = document.getElementById("opcionDestino");
   document.getElementById("imgBandera").src = destino.value;
   destinoNombre();
+
+  const fecha = new Date();
+  const añoActual = fecha.getFullYear();
+  const diaActual = fecha.getDate();
+  const mesActual = fecha.getMonth() + 1;
+  const fechaActual = añoActual + "-" + mesActual + "-" + diaActual;
+  console.log(fechaActual);
 }
 
 function horario() {
@@ -27,14 +34,14 @@ function horarioDia() {
 
   if (horarioDia.src == "https://res.cloudinary.com/anonimous/image/upload/v1661171511/train%20tickets/horario/descarga_sr7nti_n1vbo1.png") {
     let horario = "mañana";
-    return console.log(horario), (hora.innerHTML = "10 : 00 a.m");
+    return (hora.innerHTML = "10 : 00 a.m");
   } else {
     if (horarioDia.src == "https://res.cloudinary.com/anonimous/image/upload/v1661172340/train%20tickets/horario/descarga_jn6fsz_zr89co.png") {
       let horario = "tarde";
-      return console.log(horario), (hora.innerHTML = "3 : 00 p.m");
+      return (hora.innerHTML = "3 : 00 p.m");
     } else {
       let horario = "noche";
-      return console.log(horario), (hora.innerHTML = "8 : 00 p.m");
+      return (hora.innerHTML = "8 : 00 p.m");
     }
   }
 }
@@ -46,10 +53,10 @@ function destinoNombre(destinoFinal) {
     "https://res.cloudinary.com/anonimous/image/upload/v1661169566/train%20tickets/banderas/francia_ip2zud_e3dh7f.png"
   ) {
     let destinoFinal = "francea";
-    return console.log(destinoFinal);
+    return destinoFinal;
   } else {
     let destinoFinal = "londres";
-    return console.log(destinoFinal);
+    return destinoFinal;
   }
 }
 
@@ -74,4 +81,25 @@ function ramdom() {
     codigo += rango[Math.floor(Math.random() * 10)];
   }
   return console.log(codigo);
+}
+
+function dataTikect() {
+  let formData = new FormData();
+  formData.append('fecha', 'John');
+  formData.append('horario', 'John123');
+  formData.append('destinoFinal', 'John123');
+  fetch(`http://localhost:3105/comprandoTiquete?fecha=${fecha}&horario=${horario}&destinoFinal=${destinoFinal}`, {
+    method: 'get'
+  }).then(response => {
+    //handle response
+    console.log(response);
+  })
+    .then(data => {
+      //handle data
+      console.log(data);
+    })
+    .catch(error => {
+      //handle error
+      console.log(error)
+    });
 }
